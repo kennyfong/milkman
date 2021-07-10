5b18798b56cb12aaf416f88a688651d42817a301
kennyfon@gmail.com <kennyfon@gmail.com>
2021-07-10 22:04:47 +0100

    Add additional unit test for class validation

---
+++ ModernMilkman.CustomerManagement/ModernMilkman.CustomerManagement.Test/AddressClassTest.cs
@@ -0,0 +1,73 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModernMilkman.CustomerManagement.Test
+{
+    public class AddressClassTest
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        //Address Line 1 field is mandatory
+        [Test]
+        public void AddressLine1isMandatory()
+        {
+            throw new NotImplementedException();
+        }
+
+        //Address Line 1 field is up to 80 characters
+        [Test]
+        public void AddressLine1CannotbeOver80Characters()
+        {
+            throw new NotImplementedException();
+        }
+
+        //Address Line 2 field is up to 80 characters
+        [Test]
+        public void AddressLine2CannotbeOver80Characters()
+        {
+            throw new NotImplementedException();
+        }
+
+        //Town field is mandatory
+        [Test]
+        public void TownisMandatory()
+        {
+            throw new NotImplementedException();
+        }
+
+        //Town field is up to 50 characters
+        [Test]
+        public void TownCannotbeOver50Characters()
+        {
+            throw new NotImplementedException();
+        }
+
+        //County field is up to 50 characters
+        [Test]
+        public void CountyCannotbeOver50Characters()
+        {
+            throw new NotImplementedException();
+        }
+
+        //Postcode field is mandatory
+        [Test]
+        public void PostcodeisMandatory()
+        {
+            throw new NotImplementedException();
+        }
+
+        //Country defaults to UK if not provided
+        [Test]
+        public void CountryDefaultstoUKIfNotProvided()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ ModernMilkman.CustomerManagement/ModernMilkman.CustomerManagement.Test/CustomerClassTest.cs
@@ -0,0 +1,87 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModernMilkman.CustomerManagement.Test
+{
+    public class CustomerClassTest
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        //Title field is mandatory
+        [Test]
+        public void TitleisMandatory()
+        {
+            throw new NotImplementedException();
+        }
+
+        //Title field is up to 20 characters
+        [Test]
+        public void TitleCannotbeOver20Characters()
+        {
+            throw new NotImplementedException();
+        }
+
+        //Forename field is mandatory
+        [Test]
+        public void ForenameisMandatory()
+        {
+            throw new NotImplementedException();
+        }
+
+        //Forename field is up to 50 characters
+        [Test]
+        public void ForenameCannotbeOver50Characters()
+        {
+            throw new NotImplementedException();
+        }
+
+        //Surname field is mandatory
+        [Test]
+        public void SurnameisMandatory()
+        {
+            throw new NotImplementedException();
+        }
+
+        //Surname field is up to 50 characters
+        [Test]
+        public void SurnameCannotbeOver50Characters()
+        {
+            throw new NotImplementedException();
+        }
+
+        //Title field is mandatory
+        [Test]
+        public void EmailAddressisMandatory()
+        {
+            throw new NotImplementedException();
+        }
+
+        //Email Address field is up to 75 characters
+        [Test]
+        public void EmailAddressCannotbeOver75Characters()
+        {
+            throw new NotImplementedException();
+        }
+
+        //Mobile Number field is mandatory
+        [Test]
+        public void MobileNumberisMandatory()
+        {
+            throw new NotImplementedException();
+        }
+
+        //Mobile Number field is up to 15 characters
+        [Test]
+        public void MobileNumberCannotbeOver15Characters()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
