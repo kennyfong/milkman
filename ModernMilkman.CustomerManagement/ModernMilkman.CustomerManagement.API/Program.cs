2b84d5f6a918d9dd9f5702494d94adabc34cc478
kennyfong <kennyfon@gmail.com>
2021-07-10 20:49:00 +0100

    Create API project with swagger and Serilog nuget packages installed

---
+++ ModernMilkman.CustomerManagement/ModernMilkman.CustomerManagement.API/Program.cs
@@ -0,0 +1,35 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Serilog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ModernMilkman.CustomerManagement.API
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>()
+                    .UseSerilog((hostingContext, loggerConfiguration) =>
+                     {
+                         loggerConfiguration
+                             .ReadFrom.Configuration(hostingContext.Configuration)
+                             .Enrich.FromLogContext()
+                             .Enrich.WithProperty("ApplicationName", typeof(Program).Assembly.GetName().Name)
+                             .Enrich.WithProperty("Environment", hostingContext.HostingEnvironment);
+                     });
+                });
+    }
+}
